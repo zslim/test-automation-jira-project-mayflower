015c000d7ce4fed6e675368e56b1a1ecf5c225ea
Zsofia Limbek <zsofia.limbek@gmail.com>
2019-03-07 11:39:36 +0100

    documented login crash in Runner

--- src/test/java/hu/zsofi/test/jiraproject/features/Runner.java
+++ src/test/java/hu/zsofi/test/jiraproject/features/Runner.java
@@ -3,6 +3,7 @@ package hu.zsofi.test.jiraproject.features;
 import hu.zsofi.test.jiraproject.DomHandler;
 import hu.zsofi.test.jiraproject.Utils;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Runner {
@@ -16,10 +17,14 @@ public class Runner {
         logger.loginValidCredentials();
         System.out.println("submitted login data");
 
-        domHandler.waitForPageLoad();
-        driver.get("http://codecool.com");
-        domHandler.waitForElementLoad("//h1");
-        System.out.println("some content loaded");
+        domHandler.waitForPageLoad(); // TODO: show this to Márk
+        driver.get("http://codecool.com");  // TODO: if this line is after the login it breaks login but not if it's after projectMenu.click()
+//        domHandler.waitForElementLoad("//h1");
+//        System.out.println("some content loaded");
+
+        String projectMenuXpath = "//*[@id=\"browse_link\"]";
+        WebElement projectMenuLink = domHandler.waitAndGetElement(projectMenuXpath);
+        projectMenuLink.click();
 
         projectBrowser.closeDriver();
     }
